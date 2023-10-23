5c49485573f0f5565833d27403f5e8bbc1ac4f01
Bruno Oliveira <Brunooliveira.2115b@gmail.com>
2023-10-23 20:27:56 -0300

    Criado projeto firebase

---
+++ js/config-firebase.js
@@ -0,0 +1,17 @@
+    // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBd2zWOGQUGBEO0XT6jVLLbIXYmSvJmj54",
+    authDomain: "projetotarefa-4e6a6.firebaseapp.com",
+    projectId: "projetotarefa-4e6a6",
+    storageBucket: "projetotarefa-4e6a6.appspot.com",
+    messagingSenderId: "717274683024",
+    appId: "1:717274683024:web:db42f4f9d1bf1931faaa15"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
